7367bc5803eb3f53447656e2f49c91ef429aa979
Kevin Liu <gkliu93@msn.com>
2024-02-23 22:24:24 -0800

    completed part 1

--- tree_app/app/controllers/application_controller.rb
+++ tree_app/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def common_ancestor
+    permitted = params.require([:a, :b])
+    render json: Node.common_ancestor_data(permitted[0].to_i, permitted[1].to_i)
+  end
 end
--- tree_app/config/routes.rb
+++ tree_app/config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "common_ancestor" => "application#common_ancestor", as: :common_ancestor
 end
